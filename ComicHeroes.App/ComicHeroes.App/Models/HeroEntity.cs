69b7f647872d3c85655ac1f9bd88f8879de98f1e
Will Velida <willvelida@hotmail.co.uk>
2020-04-01 12:41:55 +1300

    Adding app to repo

---
+++ ComicHeroes.App/ComicHeroes.App/Models/HeroEntity.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComicHeroes.App.Models
+{
+    public class HeroEntity : TableEntity
+    {
+        public HeroEntity()
+        {
+
+        }
+
+        public HeroEntity(string heroName, string alterEgo)
+        {
+            PartitionKey = heroName;
+            RowKey = alterEgo;
+        }
+
+        public string ComicBookUniverse { get; set; }
+        public int YearFirstAppeared { get; set; }
+        public string Hometown { get; set; }
+        public string TeamAffiliation { get; set; }
+        public List<string> Powers { get; set; }
+    }
+}
